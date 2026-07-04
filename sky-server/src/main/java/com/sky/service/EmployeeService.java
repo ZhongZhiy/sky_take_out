837049671de4aff05dccf1d0aa959ecd788e223a
Zhongzhiy <sunupper.shine@gmail.com>
2026-07-04 17:03:32 +0800

    补充员工增添的重复异常处理

--- sky-server/src/main/java/com/sky/service/EmployeeService.java
+++ sky-server/src/main/java/com/sky/service/EmployeeService.java
@@ -1,8 +1,15 @@
 package com.sky.service;
 
+import com.sky.constant.PasswordConstant;
+import com.sky.constant.StatusConstant;
 import com.sky.dto.EmployeeDTO;
 import com.sky.dto.EmployeeLoginDTO;
 import com.sky.entity.Employee;
+import com.sky.mapper.EmployeeMapper;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.time.LocalDateTime;
 
 public interface EmployeeService {
 
